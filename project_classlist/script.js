de8166f6aa0912bd4639b4626b157d85b97ecb3e
Anton Yakymenko <antoshkayakymenko@gmail.com>
2021-08-29 12:02:09 +0200

    tabs

---
+++ project_classlist/script.js
@@ -0,0 +1,31 @@
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+//console.log(btns[0].classList.length);
+//console.log(btns[0].classList.item(1));
+//console.log(btns[1].classList.add('red'));
+//console.log(btns[0].classList.remove('blue'));
+//console.log(btns[0].classList.toggle('bluesadsadasd'));
+
+/* if (btns[1].classList.contains('red')) {
+    console.log('red');
+} */
+
+btns[0].addEventListener('click', () => {
+    /* if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    } */
+    btns[1].classList.toggle('red');
+});
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches("button")) {
+        console.log('HELLO');
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
